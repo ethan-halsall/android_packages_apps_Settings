4da3533bebf910d9022d5eff832f09d9af0f92c8
ethan-halsall <ethan.halsall@gmail.com>
2019-01-22 17:03:21 +0000

    AmbientDisplay: Add custom pref for devices with custom doze packages
    Signed-off-by: ethan-halsall <ethan.halsall@gmail.com>

--- src/com/android/settings/display/AmbientDisplayCustomPreferenceController.java
+++ src/com/android/settings/display/AmbientDisplayCustomPreferenceController.java
@@ -1,88 +1,74 @@
 /*
- * Copyright (C) 2017 The Android Open Source Project
+ * Copyright (C) 2016 The Android Open Source Project
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
  */
 package com.android.settings.display;
 
-import android.content.Context;
+ import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
+import android.content.ComponentName;
 import android.support.v7.preference.Preference;
 
-import com.android.internal.util.du.Utils;
-
-import com.android.settings.R;
-import com.android.settings.core.PreferenceControllerMixin;
-import com.android.settings.overlay.FeatureFactory;
-
+ import com.android.settings.R;
 import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settings.overlay.FeatureFactory;
+import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settingslib.core.instrumentation.MetricsFeatureProvider;
 
-import static com.android.internal.logging.nano.MetricsProto.MetricsEvent.ACTION_AMBIENT_DISPLAY;
+ import java.util.List;
 
-public class AmbientDisplayCustomPreferenceController extends AbstractPreferenceController implements
-        PreferenceControllerMixin {
+ import static com.android.internal.logging.nano.MetricsProto.MetricsEvent.ACTION_AMBIENT_DISPLAY;
 
-    private Context mContext;
+ public class AmbientDisplayCustomPreferenceController extends AbstractPreferenceController implements PreferenceControllerMixin {
 
-    private static final String KEY_AMBIENT_DISPLAY_CUSTOM = "ambient_display_custom";
+     static final String KEY_AMBIENT_CUSTOM = "ambient_display_custom";
 
-    private final MetricsFeatureProvider mMetricsFeatureProvider;
+     private final MetricsFeatureProvider mMetricsFeatureProvider;
 
-    public AmbientDisplayCustomPreferenceController(Context context) {
+     public AmbientDisplayCustomPreferenceController(Context context) {
         super(context);
-
         mMetricsFeatureProvider = FeatureFactory.getFactory(context).getMetricsFeatureProvider();
-        mContext = context;
     }
 
-    @Override
+     @Override
+    public boolean isAvailable() {
+        return !mContext.getResources().getString(R.string.config_customDozePackage).equals("");
+    }
+
+     @Override
     public String getPreferenceKey() {
-        return KEY_AMBIENT_DISPLAY_CUSTOM;
+        return KEY_AMBIENT_CUSTOM;
     }
 
-    @Override
+     @Override
     public boolean handlePreferenceTreeClick(Preference preference) {
-        if (KEY_AMBIENT_DISPLAY_CUSTOM.equals(preference.getKey())) {
+        if (KEY_AMBIENT_CUSTOM.equals(preference.getKey())) {
             mMetricsFeatureProvider.action(mContext, ACTION_AMBIENT_DISPLAY);
             try {
+                String[] customDozePackage = mContext.getResources().getString(R.string.config_customDozePackage).split("/");
+                String activityName = customDozePackage[0];
+                String className = customDozePackage[1];
                 Intent intent = new Intent();
-                if (Build.PRODUCT.equals("Moto G5 Plus")) {
-                    intent.setClassName(
-                            "com.dirtyunicorns.settings.device",
-                            "com.dirtyunicorns.settings.device.DozeSettingsActivity");
-                } else {
-                    intent.setClassName(
-                            "com.custom.ambient.display",
-                            "com.custom.ambient.display.DozeSettings");
-                }
-                intent.setAction("com.android.settings.action.EXTRA_SETTINGS");
+                intent.setComponent(new ComponentName(activityName, className));
                 mContext.startActivity(intent);
-            } catch (Exception ignored) {
+            } catch (Exception e){
             }
         }
         return false;
     }
 
-    @Override
-    public boolean isAvailable() {
-        return Utils.hasAltAmbientDisplay(mContext.getApplicationContext());
+     @Override
+    public void updateNonIndexableKeys(List<String> keys) {
+        keys.add(getPreferenceKey());
     }
 
-    @Override
-    public void updateState(Preference preference) {
-        super.updateState(preference);
-        preference.setSummary(R.string.custom_ambient_display_summary);
-    }
-}
+ }
